755bd7c51d667dfdb0d0bc8a0c6382ecf36b59f2
Rennan Alves Vieira <102085574+RodrigoMedeiros20@users.noreply.github.com>
2024-03-13 09:18:15 -0300

    capitulo 2

---
+++ cap02/js/resp2_a.js
@@ -0,0 +1,12 @@
+// cria referência ao form e aos elementos de resposta (pelo seu id)
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const medicamento = frm.inMedicamento.value
+    const preco = Number(frm.inPreco.value)
+
+    const promocao = preco * 2
+})
